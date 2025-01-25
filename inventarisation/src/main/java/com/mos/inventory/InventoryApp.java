349cb4f2647b61c120290f43c1c57ccffc5fec90
maj0r <kornelu123@github.com>
2025-01-25 16:27:33 +0100

    Making the app accept requests from different port

--- inventarisation/src/main/java/com/mos/inventory/InventoryApp.java
+++ inventarisation/src/main/java/com/mos/inventory/InventoryApp.java
@@ -2,12 +2,25 @@ package com.mos.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class InventoryApp {
+@EnableWebMvc
+public class InventoryApp implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApp.class, args);
+    }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Adjust this to your React app's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
